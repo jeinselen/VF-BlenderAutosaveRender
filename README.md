b0c3cfd19ee681fcedc71eadbd767d2dbe926636
John Einselen <jeinselen@vectorform.com>
2021-05-26 17:43:16 -0400

    Added render time feature
    Added rendering duration calculation, removed render engine check, and implemented general refinements to the entire codebase

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # VF-BlenderAutoSaveRender
-Automatically saves a numbered or dated image after every render
+Automatically saves a numbered or dated image after every render. This Blender Addon is designed to make test renders easier to review, saving what would otherwise be lost when quitting the app. It's also good for showing render progression after a project is complete, a timelapse of sorts.
+
+### Installation and usage:
+- Download the .py file
+- Open up Blender preferences
+- Install the addon
+- Enable the addon
+- Open up the Render Output settings to find the Auto Save Render sub-panel and enable it
+
+![screenshot of the Blender render output user interface with the addon installed](images/screenshot.jpg)
+
+### Settings:
+- Custom Save Directory
+  - Leave a single forward slash `/` to use an auto-generated folder with the same name as the current project
+  - Select a specific directory to save all files to the same location
+- Camera Name
+  - Includes the name of the currently active camera in the output file name
+  - _Note: the file name always starts with the same name as the project, regardless of these checkboxes_
+- Frame Number
+  - Includes the currently active frame padded to 5 digits
+- Render Engine
+  - Includes the internal name of the currently active render engine
+- Render Time
+  - Calculates the time spent rendering and includes it in the file name
+  - _Note: this addon will only save the final frame during animation sequences to prevent unnecessary duplication (animations should be saved already), but the render time will reflect how long it took to complete the entire animation so if you want to calculate the total time spent rendering tests and animations for a project, you can use these numbers_
+  - _Note: the calculated render time may vary by 0.01 second from the time recorded by Blender due to Python API limitations_
+- File Numbering
+  - A unique number is always included to prevent overwriting previous renders, with either a _Serial Number_ padded to 4 digits, or a _Time Stamp_ using the format `YYYY_MM_DD-hour_minute_second`
+- File Format
+  - This can be set to use the same format as selected in the Blender Output panel, or override this project setting with PNG, JPEG, or OpenEXR MultiLayer output
--- VF_auto_save_render.py
+++ VF_auto_save_render.py
@@ -1,13 +1,13 @@
 bl_info = {
 	"name": "VF Auto Save Render",
-	"author": "John Einselen - Vectorform LLC, based on work by tstscr(florianfelix)",
-	"version": (0, 4),
+	"author": "John Einselen - Vectorform LLC, based on original work by tstscr(florianfelix)",
+	"version": (0, 5),
 	"blender": (2, 80, 0),
 	"location": "Rendertab > Output Panel > Subpanel",
 	"description": "Automatically saves numbered or dated images in a directory alongside the project file or in a custom location",
-	# "description": "Automatically saves a numbered or dated image after every render is completed or canceled",
 	"warning": "inexperienced developer, use at your own risk",
-	"wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/Scripts/Render/Auto_Save",
+	"wiki_url": "",
+	"tracker_url": "",
 	"category": "Render"}
 
 # Based on the following resources:
@@ -15,9 +15,11 @@ bl_info = {
 # https://github.com/patrickhill/blender-datestamper/blob/master/render_auto_save_with_datestamp.py
 # https://gist.github.com/robertguetzkow/8dacd4b565538d657b72efcaf0afe07e
 # https://blender.stackexchange.com/questions/6842/how-to-get-the-directory-of-open-blend-file-from-python
+# https://github.com/AlreadyLegendary/Render-time-estimator
 
 import os
 import datetime
+import time
 import bpy
 from bpy.app.handlers import persistent
 from re import findall, search
@@ -51,63 +53,67 @@ IMAGE_EXTENSIONS = (
 	'tif'
 )
 
+###########################################################################
+# Auto save render function
+
 @persistent
 def auto_save_render(scene):
 	if not bpy.context.scene.auto_save_render_settings.enable_auto_save_render or not bpy.data.filepath:
 		return
 	rndr = scene.render
+
+	# Calculate elapsed render time
+	render_duration = round(time.time() - float(bpy.context.scene.auto_save_render_settings.start_date), 2)
+
+	# Save original file format settings
 	original_format = rndr.image_settings.file_format
 	original_colormode = rndr.image_settings.color_mode
 	original_colordepth = rndr.image_settings.color_depth
 
-	if bpy.context.scene.auto_save_render_settings.auto_save_format == 'SCENE':
+	# Set up render output formatting
+	if bpy.context.scene.auto_save_render_settings.file_format == 'SCENE':
 		if original_format not in IMAGE_FORMATS:
 			print('{} Format is not an image format. Not Saving'.format(
 				original_format))
 			return
-	elif bpy.context.scene.auto_save_render_settings.auto_save_format == 'PNG':
-		rndr.image_settings.file_format = 'PNG'
-	elif bpy.context.scene.auto_save_render_settings.auto_save_format == 'OPEN_EXR_MULTILAYER':
-		rndr.image_settings.file_format = 'OPEN_EXR_MULTILAYER'
-	elif bpy.context.scene.auto_save_render_settings.auto_save_format == 'JPEG':
+	elif bpy.context.scene.auto_save_render_settings.file_format == 'JPEG':
 		rndr.image_settings.file_format = 'JPEG'
+	elif bpy.context.scene.auto_save_render_settings.file_format == 'PNG':
+		rndr.image_settings.file_format = 'PNG'
+	elif bpy.context.scene.auto_save_render_settings.file_format == 'OPEN_EXR_MULTILAYER':
+		rndr.image_settings.file_format = 'OPEN_EXR_MULTILAYER'
 	extension = rndr.file_extension
 
 	# Set location and file name variables
-	# blendname = basename(bpy.data.filepath).rpartition('.')[0]
-	# blendname = os.path.splitext(os.path.basename(bpy.data.filepath))[0]
 	blendname = os.path.splitext(os.path.basename(bpy.data.filepath))[0].replace(' ', '')
 
-	# filepath = os.path.join(os.path.dirname(bpy.data.filepath), blendname)
-	if len(bpy.context.scene.auto_save_render_settings.auto_save_customdirectory) <= 1:
+	if len(bpy.context.scene.auto_save_render_settings.customdirectory) <= 1:
 		filepath = os.path.join(os.path.dirname(bpy.data.filepath), blendname)
-		# filepath = os.path.splitext(bpy.data.filepath)[0]
 	else:
-		filepath = bpy.context.scene.auto_save_render_settings.auto_save_customdirectory
+		filepath = bpy.context.scene.auto_save_render_settings.customdirectory
 
-	# Creates the project subfolder if it doesn't already exist
+	# Create the project subfolder if it doesn't already exist
 	if not os.path.exists(filepath):
 		os.mkdir(filepath)
 
 	# Build initial output file name components
 	save_name = blendname
-	if bpy.context.scene.auto_save_render_settings.auto_save_include_cameraname:
-		# save_name += '-' + bpy.context.scene.camera.name
+	if bpy.context.scene.auto_save_render_settings.include_cameraname:
 		save_name += '-' + bpy.context.scene.camera.name.replace(' ', '')
-	if bpy.context.scene.auto_save_render_settings.auto_save_include_framenumber:
-		# save_name += '-' + str(bpy.context.scene.frame_current).zfill(5)
+	if bpy.context.scene.auto_save_render_settings.include_framenumber:
 		save_name += '-' + format(bpy.context.scene.frame_current, '05')
-		# save_name += '-frame' + format(bpy.context.scene.frame_current, '05')
-	if bpy.context.scene.auto_save_render_settings.auto_save_include_renderengine:
+	if bpy.context.scene.auto_save_render_settings.include_renderengine:
 		save_name += '-' + bpy.context.engine.replace('BLENDER_', '')
+	if bpy.context.scene.auto_save_render_settings.include_rendertime:
+		save_name += '-' + str(render_duration)
 
 	# Generate the serial number
-	# Finds all of the image files that start with blendname
+		# Finds all of the image files in the selected directory that start with blendname
 	files = [f for f in os.listdir(filepath)
 			if f.startswith(blendname)
 			and f.lower().endswith(IMAGE_EXTENSIONS)]
 
-	# Searches the file collection and returns the next highest number as a 4 digit string
+		# Searches the file collection and returns the next highest number as a 4 digit string
 	def save_number_from_files(files):
 		highest = 0
 		if files:
@@ -120,13 +126,14 @@ def auto_save_render(scene):
 		return str(highest+1).zfill(4)
 
 	# Finish building the output file name
-	if bpy.context.scene.auto_save_render_settings.auto_save_numbering == 'SERIAL':
+	if bpy.context.scene.auto_save_render_settings.numbering_type == 'SERIAL':
 		save_name += '-' + save_number_from_files(files)
 	else:
 		save_name += '-' + datetime.datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
 	save_name += extension
 	save_name = os.path.join(filepath, save_name)
 
+	# Save image file
 	image = bpy.data.images['Render Result']
 	if not image:
 		print('Auto Save: Render Result not found. Image not saved')
@@ -135,28 +142,39 @@ def auto_save_render(scene):
 	print('Auto_Save:', save_name)
 	image.save_render(save_name, scene=None)
 
+	# Restore original user settings for render output (otherwise a temporary JPEG format above will overwrite the final PNG output colour mode and depth settings)
 	rndr.image_settings.file_format = original_format
 	rndr.image_settings.color_mode = original_colormode
 	rndr.image_settings.color_depth = original_colordepth
 
 ###########################################################################
+# Start time function
+
+@persistent
+def render_start_time(scene):
+	# Saves start time in seconds as a string to the addon settings
+	bpy.context.scene.auto_save_render_settings.start_date = str(time.time())
+
+###########################################################################
+# UI input functions
 
 def set_directory(self, value):
 	path = Path(value)
 	if path.is_dir():
-		self["auto_save_customdirectory"] = value
+		self["customdirectory"] = value
 
 def get_directory(self):
-	return self.get("auto_save_customdirectory", bpy.context.scene.auto_save_render_settings.bl_rna.properties["auto_save_customdirectory"].default)
+	return self.get("customdirectory", bpy.context.scene.auto_save_render_settings.bl_rna.properties["customdirectory"].default)
 
 ###########################################################################
+# UI settings and rendering classes
 
 class AutoSaveRenderSettings(bpy.types.PropertyGroup):
 	enable_auto_save_render: bpy.props.BoolProperty(
 		name="Enable/disable automatic saving of rendered images",
 		description="Automatically saves numbered or dated images in a directory alongside the project file or in a custom location",
 		default=False)
-	auto_save_customdirectory: bpy.props.StringProperty(
+	customdirectory: bpy.props.StringProperty(
 		name="Custom Save Directory",
 		description="Leave a single forward slash to auto generate folders alongside the project file",
 		default="/",
@@ -164,23 +182,23 @@ class AutoSaveRenderSettings(bpy.types.PropertyGroup):
 		subtype="DIR_PATH",
 		set=set_directory,
 		get=get_directory)
-	auto_save_include_cameraname: bpy.props.BoolProperty(
+	include_cameraname: bpy.props.BoolProperty(
 		name="Camera Name",
 		description="Include the camera name in the auto saved file name",
 		default=True)
-	auto_save_include_framenumber: bpy.props.BoolProperty(
+	include_framenumber: bpy.props.BoolProperty(
 		name="Frame Number",
 		description="Include the frame number in the auto saved file name",
 		default=True)
-	auto_save_include_renderengine: bpy.props.BoolProperty(
+	include_renderengine: bpy.props.BoolProperty(
 		name="Render Engine",
 		description="Include the current render engine code in the auto saved file name",
 		default=False)
-	auto_save_include_rendertime: bpy.props.BoolProperty(
+	include_rendertime: bpy.props.BoolProperty(
 		name="Render Time",
 		description="Include the number of seconds spent rendering in the auto saved file name",
 		default=False)
-	auto_save_numbering: bpy.props.EnumProperty(
+	numbering_type: bpy.props.EnumProperty(
 		name='File Numbering',
 		description='Serial or time stamp, ensuring every file is saved without overwriting',
 		items=[
@@ -189,7 +207,7 @@ class AutoSaveRenderSettings(bpy.types.PropertyGroup):
 			('TIME', 'Time Stamp', 'Saves images with the local date and time'),
 			],
 		default='SERIAL')
-	auto_save_format: bpy.props.EnumProperty(
+	file_format: bpy.props.EnumProperty(
 		name='File Format',
 		description='Image format used for the automatically saved render files',
 		items=[
@@ -199,6 +217,10 @@ class AutoSaveRenderSettings(bpy.types.PropertyGroup):
 			('OPEN_EXR_MULTILAYER', 'OpenEXR MultiLayer', 'Save as multilayer exr'),
 			],
 		default='JPEG')
+	start_date: bpy.props.StringProperty(
+		name="Render Start Date",
+		description="Stores the date as a string for when rendering began",
+		default="")
 
 class RENDER_PT_auto_save_render(bpy.types.Panel):
 	bl_space_type = 'PROPERTIES'
@@ -207,11 +229,13 @@ class RENDER_PT_auto_save_render(bpy.types.Panel):
 	bl_label = "Auto Save Render"
 	bl_parent_id = "RENDER_PT_output"
 	bl_options = {'DEFAULT_CLOSED'}
-	compatible_render_engines = {'BLENDER_RENDER', 'BLENDER_OPENGL', 'BLENDER_WORKBENCH', 'BLENDER_EEVEE', 'CYCLES', 'RPR', 'LUXCORE'}
 
-	@classmethod
-	def poll(cls, context):
-		return (context.engine in cls.compatible_render_engines)
+	# Check for engine compatibility
+	# This is currently disabled for simplicity
+	# compatible_render_engines = {'BLENDER_RENDER', 'BLENDER_OPENGL', 'BLENDER_WORKBENCH', 'BLENDER_EEVEE', 'CYCLES', 'RPR', 'LUXCORE'}
+	# @classmethod
+	# def poll(cls, context):
+		# return (context.engine in cls.compatible_render_engines)
 
 	def draw_header(self, context):
 		self.layout.prop(context.scene.auto_save_render_settings, 'enable_auto_save_render', text='')
@@ -219,30 +243,31 @@ class RENDER_PT_auto_save_render(bpy.types.Panel):
 	def draw(self, context):
 		layout = self.layout
 		layout.use_property_decorate = False  # No animation
-		layout.prop(context.scene.auto_save_render_settings, 'auto_save_customdirectory', text='')
+		layout.prop(context.scene.auto_save_render_settings, 'customdirectory', text='')
 
 		layout.use_property_split = True
 		col = layout.column(align=True)
 		row = col.row(align=True)
-		row.prop(context.scene.auto_save_render_settings, 'auto_save_include_cameraname')
-		row.prop(context.scene.auto_save_render_settings, 'auto_save_include_framenumber')
-		row.prop(context.scene.auto_save_render_settings, 'auto_save_include_renderengine')
-		# row.prop(context.scene.auto_save_render_settings, 'auto_save_include_rendertime')
+		row.prop(context.scene.auto_save_render_settings, 'include_cameraname')
+		row.prop(context.scene.auto_save_render_settings, 'include_framenumber')
+		row.prop(context.scene.auto_save_render_settings, 'include_renderengine')
+		row.prop(context.scene.auto_save_render_settings, 'include_rendertime')
 
-		# Until render time is added to the Python API in Blender, or a timing system is built, this will be disabled
-		subrow = row.column(align=True)
-		subrow.active = False
-		subrow.prop(context.scene.auto_save_render_settings, 'auto_save_include_rendertime')
-
-		layout.prop(context.scene.auto_save_render_settings, 'auto_save_numbering')
-		layout.prop(context.scene.auto_save_render_settings, 'auto_save_format')
+		layout.prop(context.scene.auto_save_render_settings, 'numbering_type')
+		layout.prop(context.scene.auto_save_render_settings, 'file_format')
 
 classes = (AutoSaveRenderSettings, RENDER_PT_auto_save_render)
+
+###########################################################################
+# Addon registration functions
 
 def register():
 	for cls in classes:
 		bpy.utils.register_class(cls)
 	bpy.types.Scene.auto_save_render_settings = bpy.props.PointerProperty(type=AutoSaveRenderSettings)
+	# Using init instead of pre means that the entire animation render time is tracked instead of just the final frame
+	# bpy.app.handlers.render_pre.append(render_start_time)
+	bpy.app.handlers.render_init.append(render_start_time)
 	# Using cancel and complete, instead of render_post, prevents saving an image for every frame in an animation
 	# bpy.app.handlers.render_post.append(auto_save_render)
 	bpy.app.handlers.render_cancel.append(auto_save_render)
@@ -252,6 +277,9 @@ def unregister():
 	for cls in reversed(classes):
 		bpy.utils.unregister_class(cls)
 	del bpy.types.Scene.auto_save_render_settings
+	# Using init instead of pre means that the entire animation render time is tracked instead of just the final frame
+	# bpy.app.handlers.render_pre.remove(render_start_time)
+	bpy.app.handlers.render_init.remove(render_start_time)
 	# Using cancel and complete, instead of render_post, prevents saving an image for every frame in an animation
 	# bpy.app.handlers.render_post.remove(auto_save_render)
 	bpy.app.handlers.render_cancel.remove(auto_save_render)
